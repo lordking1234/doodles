3b71bf612751238505ef2855697b59cdeb22514e
lordking1234 <92648020+lordking1234@users.noreply.github.com>
2023-01-07 11:43:25 +0330

    Update 1_🪙_Mint_Process.py

--- pages/1_🪙_Mint_Process.py
+++ pages/1_🪙_Mint_Process.py
@@ -29,7 +29,7 @@ hi = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://img.freepik.com/premium-vector/blue-white-gray-turquoise-background-gradient-wallpaper-background-vector-illustration_172010-1295.jpg?w=360");
-background-size: 360%;
+background-size: 420%;
 background-position: top right;
 background-repeat: no-repeat;
 background-attachment: local;
